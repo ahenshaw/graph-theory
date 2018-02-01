53c0010516815082ec83d020d5b577bb45fff49c
Andrew Henshaw <andrew@henshaw.us>
2018-02-01 14:57:28 -0500

    added vertex cover

--- dominating.py
+++ dominating.py
@@ -9,21 +9,3 @@ def findMinimalDominatingSet(g):
                 return c
     return {}
 
-for name, fn in [('Path Graph', nx.path_graph),
-                 ('Cycle Graph', nx.cycle_graph),
-                 ('Complete Graph', nx.complete_graph),
-                 ('Circular Ladder Graph', nx.circular_ladder_graph)]:
-    
-    
-    print('\n'+name)
-    for n in range(2, 13):
-        g = fn(n)
-        s = findMinimalDominatingSet(g)
-        print('    n={:2}:  {:2}  {}'.format(n, len(s), s))
-
-print('\nComplete Bipartite')
-for m in range(2, 7):
-    for n in range(2, 7):
-        g = nx.complete_bipartite_graph(m, n)
-        s = findMinimalDominatingSet(g)
-        print('    m={} n={}:  {:2}  {}'.format(m, n, len(s), s))
